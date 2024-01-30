d14aad7fa111e35cda16f055a37ed3d21398a3e7
Andrew <andrew.j.b.chapmanb@gmail.com>
2024-01-30 13:40:27 -0800

    Initial Commit

---
+++ Irwindale-speedway3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Irwindale-speedway3
+//
+//  Created by Andrew Chapman on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Irwindale-speedway3/Irwindale_speedway3App.swift
@@ -0,0 +1,17 @@
+//
+//  Irwindale_speedway3App.swift
+//  Irwindale-speedway3
+//
+//  Created by Andrew Chapman on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Irwindale_speedway3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
